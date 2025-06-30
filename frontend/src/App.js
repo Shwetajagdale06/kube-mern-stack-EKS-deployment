d378cd4e6e9fc8d77fa52fae1305fb08d7f36d41
Shweta Jagdale <120297994+Shwetajagdale06@users.noreply.github.com>
2025-06-30 19:45:21 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div>
-      <h1>Add User....(from Git Rollback.............) </h1>
+      <h1>Add User.... </h1>
       <input value={name} onChange={e => setName(e.target.value)} />
       <button onClick={addUser}>Add</button>
       <ul>
